a1e3278b3841225b6953632734f57bcedc66f5ea
CreatorB <creatorb45@gmail.com>
2015-04-05 11:16:38 +0700

    add splashscreen

--- android/AndroidImageGallery/app/src/main/java/imagegalerry/creatorb/id/androidimagegallery/Splash.java
+++ android/AndroidImageGallery/app/src/main/java/imagegalerry/creatorb/id/androidimagegallery/Splash.java
@@ -1,39 +1,37 @@
 package imagegalerry.creatorb.id.androidimagegallery;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Splash extends ActionBarActivity {
+public class Splash extends Activity {
+
+    //set durasi selama 3 detik
+    private final int SPLASH_DURATION = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        //buat handler baru untuk memulai activity selanjutnya
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //pasang intent untuk menuju activity selanjutnya
+                Intent next = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(next);
+                //akhiri activity splash
+                finish();
+            }
+        }, SPLASH_DURATION);
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
