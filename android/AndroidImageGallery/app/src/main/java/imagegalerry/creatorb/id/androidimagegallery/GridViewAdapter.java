83c997b3daf1871df7d8f3929d96f37a5d5c1d98
CreatorB <creatorb45@gmail.com>
2015-04-05 20:53:36 +0700

    reformat code

--- android/AndroidImageGallery/app/src/main/java/imagegalerry/creatorb/id/androidimagegallery/GridViewAdapter.java
+++ android/AndroidImageGallery/app/src/main/java/imagegalerry/creatorb/id/androidimagegallery/GridViewAdapter.java
@@ -4,16 +4,11 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.text.StaticLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
-
-import java.util.Objects;
-import java.util.Vector;
 
 /**
  * Created by creatorbe on 3/22/15.
--- android/AndroidImageGallery/app/src/main/java/imagegalerry/creatorb/id/androidimagegallery/Splash.java
+++ android/AndroidImageGallery/app/src/main/java/imagegalerry/creatorb/id/androidimagegallery/Splash.java
@@ -2,11 +2,8 @@ package imagegalerry.creatorb.id.androidimagegallery;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
 
 
 public class Splash extends Activity {
